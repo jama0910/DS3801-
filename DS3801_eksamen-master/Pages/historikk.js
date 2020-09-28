aca3b1ec9d9af3a6ac51c2eb536c978c95bf0bc2
mohjava0910 <44406954+jama0910@users.noreply.github.com>
2020-09-28 15:43:13 +0200

    DS3801 eksamen

---
+++ DS3801_eksamen-master/Pages/historikk.js
@@ -0,0 +1,17 @@
+function showAkupunktur() {
+    document.getElementById("avbestilling").style.display ="flex";
+}
+
+function showOsteopati() {
+    document.getElementById("avbestilling2").style.display ="flex";
+}
+
+function hideAkupunktur() {
+    document.getElementById("avbestilling").style.display ="none";
+    document.getElementById("akupunktur").style.display ="none";
+}
+
+function hideOsteopati() {
+    document.getElementById("avbestilling2").style.display ="none";
+    document.getElementById("osteopati").style.display ="none";
+}
